4a0260d602557b4fe412746383b704161bb88a1c
lmintsys <lubmin23@gmail.com>
2023-05-18 18:06:57 -0400

    added functions to add and delete notes

--- server.js
+++ server.js
@@ -6,3 +6,37 @@ const { notes } = require("./db/db.json");
 
 const app = express();
 const PORT = 3001;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Delete a note
+function updateDb(id, notesArray) {
+  const deletedNote = id;
+  for (let i = 0; i < notesArray.length; i++) {
+    if (deletedNote === notesArray[i].id) {
+      notesArray.splice(i, 1);
+      fs.writeFileSync(
+        path.join(__dirname, "/db/db.json"),
+        JSON.stringify({ notes: notesArray }, null, 2),
+        (err) => {
+          if (err) {
+            throw err;
+          }
+        }
+      );
+    }
+  }
+}
+
+// Create a note
+function createNewNote(body, notesArray) {
+  const newNote = body;
+  notesArray.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, "/db/db.json"),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  return newNote;
+}
